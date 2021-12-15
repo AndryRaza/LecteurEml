4578b80a4d5d319b423a3c0c26c036f0d1c69c8b
A <razaandry@hotmail.com>
2021-12-15 12:28:07 +0400

    Ajout content type

--- LecteurEmlPropre/Details.xaml.cs
+++ LecteurEmlPropre/Details.xaml.cs
@@ -24,6 +24,14 @@ namespace LecteurEmlPropre
             get;
             set;
         }
+
+        public string Text
+        {
+            get;
+            set;
+        }
+
+
         public Details()
         {
             InitializeComponent();
@@ -39,9 +47,60 @@ namespace LecteurEmlPropre
         private void readEmlRaw(string path)
         {
             System.IO.StreamReader sr = new System.IO.StreamReader(path);
-            string Text = sr.ReadToEnd();
+            Text = sr.ReadToEnd();
             sr.Close();
-            contenu.Text = Text;
+            traitement();
+        }
+
+        private void traitement()
+        {
+            string txtContenttype = "";
+
+            string[] arr = Text.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+
+            foreach (string str in arr)
+            {
+
+                if (str.StartsWith("Content-Type"))
+                {
+                    if (txtContenttype == "")
+                    {
+                        txtContenttype += extractContentType(str);
+                    }
+                    else
+                    {
+                        txtContenttype += "\r\n" + extractContentType(str);
+                    }
+                }
+            }
+
+            contentType.Text = txtContenttype;
+        }
+
+        private string extractContentType(string str)
+        {
+            string[] arr = str.Split(' ');
+
+            arr = delete(1, arr);
+
+
+            string txtContenttype = String.Join("", arr);
+
+            return txtContenttype;
+        }
+
+        //Supprimer un élément dans une liste
+        private string[] delete(int pos, string[] arr)
+        {
+            string[] provi = arr;
+
+            for (int i = pos - 1; i < provi.Length - 1; i++)
+            {
+                Console.WriteLine(i);
+                provi[i] = provi[i + 1];
+            }
+
+            return provi;
         }
 
     }
